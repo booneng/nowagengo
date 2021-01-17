c4f805a2e1c5c105ea7b24bd4199b27cb68c1884
booneng <ohbooneng@gmail.com>
2021-01-17 16:59:50 +0800

    pull and generate go proto

--- protobuf/README.md
+++
@@ -1,5 +0,0 @@
-# Nowa proto
-
-This repository includes the protobuf service definition for Nowa backend
-service and the generated protobuf code exported as packages for different
-languages.
--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -4,6 +4,10 @@ ROOT="$(cd "$(dirname "$0")/.." &>/dev/null; pwd -P)"
 
 PROTOC_GO_CONTAINER_IMAGE="booneng/protoc-go"
 
+git clone https://github.com/booneng/nowaproto
+
+mkdir nowaservice
+
 docker build -t "${PROTOC_GO_CONTAINER_IMAGE}" .
 
 docker run \
@@ -12,9 +16,10 @@ docker run \
     --volume "${ROOT}:${ROOT}" \
     --workdir "${ROOT}" \
     "${PROTOC_GO_CONTAINER_IMAGE}" \
-        --proto_path=${ROOT}/protobuf/nowaservice \
+        --proto_path=${ROOT}/nowaproto/nowaservice \
         --go_out=plugins=grpc,paths=source_relative:./nowaservice \
-        ${ROOT}/protobuf/nowaservice/*.proto
+        ${ROOT}/nowaproto/nowaservice/*.proto
 
+rm -rf nowaproto
 
 echo "Protos regenerated (OK)"
