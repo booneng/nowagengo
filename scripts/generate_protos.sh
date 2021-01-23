7be9f64a5d3cb4b5cf41c752dc6e88edf8d2ff7d
booneng <ohbooneng@gmail.com>
2021-01-23 18:14:30 +0800

    use submodules

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -4,10 +4,6 @@ ROOT="$(cd "$(dirname "$0")/.." &>/dev/null; pwd -P)"
 
 PROTOC_GO_CONTAINER_IMAGE="booneng/protoc-go"
 
-git clone https://github.com/booneng/nowaproto
-
-mkdir nowaservice
-
 docker build -t "${PROTOC_GO_CONTAINER_IMAGE}" .
 
 docker run \
@@ -19,7 +15,5 @@ docker run \
         --proto_path=${ROOT}/nowaproto/nowaservice \
         --go_out=plugins=grpc,paths=source_relative:./nowaservice \
         ${ROOT}/nowaproto/nowaservice/*.proto
-
-rm -rf nowaproto
 
 echo "Protos regenerated (OK)"
